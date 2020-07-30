8b57ca005de8f0e2174e44d0b9299bea808058ab
fsiddh <jasonshaw546@gmail.com>
2020-07-30 16:01:26 +0530

    Updates

--- usingScrapy/demo.py
+++ usingScrapy/demo.py
@@ -15,8 +15,7 @@ if __name__ == "__main__":
     driver_obj = webdriver.Chrome(chromedriver_path)
     driver_obj.maximize_window()
     driver_obj.get("https://www.facebook.com")
-    user_detail = get_user_name_password()
-
+    user_detail = get_credentials()
     username_element = driver_obj.find_element_by_id("email")
     password_element = driver_obj.find_element_by_id("pass")
     submit_btn_element = driver_obj.find_element_by_id("loginbutton")
@@ -26,3 +25,19 @@ if __name__ == "__main__":
     submit_btn_element.click()
     wait = WebDriverWait(driver_obj, 5)
 
+    print(driver_obj.title)
+
+    # profile_name_list = driver_obj.find_elements_by_class_name("profileLink")
+    # for profile_name in profile_name_list:
+    #     print("profile_name: " + profile_name.text)
+
+    driver_obj.get("https://www.facebook.com/groups/214914325234824/")
+
+    profile_name_list = driver_obj.find_elements_by_class_name("profileLink")
+    wait = WebDriverWait(driver_obj, 50)
+    for profile_name in profile_name_list:
+        print("profile_name: " + profile_name.text)
+        print(profile_name.get_attribute("href"))
+
+    driver_obj.close()
+    print("Web Scrapping Application Completed.")
