4e9a47aabe98e67723063386b54ad319e6b73332
fsiddh <jasonshaw546@gmail.com>
2020-07-30 14:52:05 +0530

    updates

---
+++ demo.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+if __name__ == '__main__':
+    print('Web Scrapping Applicatin Started!')
+    chromedriver_path = r"D:\Work\Webscrapper\chromedriver.exe"
+    driver = webdriver.Chrome(chromedriver_path)
+    driver.maximize_window()
+    driver.get("https://www.google.com")
+
+    print(driver.title)
+
+    driver.close()
+    print("Web Scrapping Application Completed!")
