a071473801fa9482561e8e095fdb7c83ee1273bf
fsiddh <jasonshaw546@gmail.com>
2020-07-30 15:41:35 +0530

    updates

--- demo.py
+++ demo.py
@@ -1,13 +1,28 @@
+import json
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 
-if __name__ == '__main__':
-    print('Web Scrapping Applicatin Started!')
+def get_credentials():
+    config_data = None
+    with open('readme.json') as json_file:
+        config_data = json.load(json_file)
+    return config_data['facebook']       
+
+
+if __name__ == "__main__":
+    print("Web Scrapping Application Started ...")
     chromedriver_path = r"D:\Work\Webscrapper\chromedriver.exe"
-    driver = webdriver.Chrome(chromedriver_path)
-    driver.maximize_window()
-    driver.get("https://www.google.com")
+    driver_obj = webdriver.Chrome(chromedriver_path)
+    driver_obj.maximize_window()
+    driver_obj.get("https://www.facebook.com")
+    user_detail = get_user_name_password()
 
-    print(driver.title)
+    username_element = driver_obj.find_element_by_id("email")
+    password_element = driver_obj.find_element_by_id("pass")
+    submit_btn_element = driver_obj.find_element_by_id("loginbutton")
 
-    driver.close()
-    # print("Web Scrapping Application Completed!")
+    username_element.send_keys(user_detail["user_name"])
+    password_element.send_keys(user_detail["password"])
+    submit_btn_element.click()
+    wait = WebDriverWait(driver_obj, 5)
+
