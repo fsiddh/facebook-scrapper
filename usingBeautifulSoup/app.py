0d5f34e5934ebb1402596e71821e6c978288b688
fsiddh <jasonshaw546@gmail.com>
2020-07-31 09:20:56 +0530

    Update 0.6

--- usingBeautifulSoup/app.py
+++ usingBeautifulSoup/app.py
@@ -4,8 +4,8 @@ import json
 import time
 import logging
 import pandas
-from collections import OrderedDict
 from bs4 import BeautifulSoup
+from collections import OrderedDict
 
 class Facebook:
 
@@ -44,10 +44,104 @@ class Facebook:
                 break
         return BeautifulSoup(r.text, 'lxml')
 
+    def extract_comments(self, session, base_url, post_bs, post_url):
+
+        # Extracts all coments from post
+        comments = list()
+        show_more_url = post_bs.find('a', href=re.compile('/story\.php\?story'))['href']
+        first_comment_page = True
+
+        logging.info('Scraping comments from {}'.format(post_url))
+        while True:
+
+            logging.info('[!] Scraping comments.')
+            time.sleep(3)
+            if first_comment_page:
+                first_comment_page = False
+            else:
+                post_bs = self.get_bs(session, base_url+show_more_url)
+                time.sleep(3)
+            
+            try:
+                comments_elements = post_bs.find('div', id=re.compile('composer')).next_sibling\
+                    .find_all('div', id=re.compile('^\d+'))
+            except Exception:
+                pass
+
+            if len(comments_elements) != 0:
+                logging.info('[!] There are comments.')
+            else:
+                break
+            
+            for comment in comments_elements:
+                comment_data = OrderedDict()
+                comment_data['text'] = list()
+                try:
+                    comment_strings = comment.find('h3').next_sibling.strings
+                    for string in comment_strings:
+                        comment_data['text'].append(string)
+                except Exception:
+                    pass
+                
+                try:
+                    media = comment.find('h3').next_sibling.next_sibling.children
+                    if media is not None:
+                        for element in media:
+                            comment_data['media_url'] = element['src']
+                    else:
+                        comment_data['media_url'] = ''
+                except Exception:
+                    pass
+                
+                comment_data['profile_name'] = comment.find('h3').a.string
+                comment_data['profile_url'] = comment.find('h3').a['href'].split('?')[0]
+                comments.append(dict(comment_data))
+            
+            show_more_url = post_bs.find('a', href=re.compile('/story\.php\?story'))
+            if 'View more' in show_more_url.text:
+                logging.info('[!] More comments.')
+                show_more_url = show_more_url['href']
+            else:
+                break
+        
+        return comments
+
+    def scrape_post(self, session, base_url, post_url):
+
+        # Goes to post URL and extracts post data.
+        post_data = OrderedDict()
+
+        post_bs = self.get_bs(session, base_url+post_url)
+        time.sleep(5)
+
+        # Here we populate the OrderedDict object
+        post_data['url'] = post_url
+
+        try:
+            post_text_element = post_bs.find('div', id='u_0_0').div
+            string_groups = [p.strings for p in post_text_element.find_all('p')]
+            strings = [repr(string) for group in string_groups for string in group]
+            post_data['text'] = strings
+        except Exception:
+            post_data['text'] = []
+        
+        try:
+            post_data['media_url'] = post_bs.find('div', id='u_0_0').find('a')['href']
+        except Exception:
+            post_data['media_url'] = ''
+        
+
+        try:
+            post_data['comments'] = self.extract_comments(session, base_url, post_bs, post_url)
+        except Exception:
+            post_data['comments'] = []
+        
+        return dict(post_data)
+
     def crawl_profile(self, session, base_url, profile_url, post_limit):
 
         # Goes to profile URL, crawls it and extracts posts URLs.
-        profile_bs = get_bs(session, profile_url)
+        profile_bs = self.get_bs(session, profile_url)
         n_scraped_posts = 0
         scraped_posts = list()
         posts_id = None
@@ -65,23 +159,22 @@ class Facebook:
             for post_url in posts_urls:
                 # print(post_url)
                 try:
-                    post_data = scrape_post(session, base_url, post_url)
+                    post_data = self.scrape_post(session, base_url, post_url)
                     scraped_posts.append(post_data)
                 except Exception as e:
                     logging.info('Error: {}'.format(e))
                 n_scraped_posts += 1
-                if posts_completed(scraped_posts, post_limit):
+                if self.posts_completed(scraped_posts, post_limit):
                     break
             
             show_more_posts_url = None
-            if not posts_completed(scraped_posts, post_limit):
+            if not self.posts_completed(scraped_posts, post_limit):
                 show_more_posts_url = profile_bs.find('div', id=posts_id).next_sibling.a['href']
-                profile_bs = get_bs(session, base_url+show_more_posts_url)
+                profile_bs = self.get_bs(session, base_url+show_more_posts_url)
                 time.sleep(3)
             else:
                 break
         return scraped_posts
-
 
 
 
