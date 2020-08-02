bd24bd3faa160ffa8341637e330aabefbd1eb602
fsiddh <jasonshaw546@gmail.com>
2020-08-02 09:46:20 +0530

    updates

--- Mining different kinds of data from fb/my_profile.py
+++ Mining different kinds of data from fb/my_profile.py
@@ -5,7 +5,7 @@ import json
 import facebook
 
 if __name__ == '__main__':
-	token = 'EAANcgmchBvsBAByxcHHnZChe0jXeTZCcxZArqaIWAsbgCZBACCK10SM9bMWMIX0NYUnkqTDGjFrZAQpgK3XeJejPNtBnv60IpEDWmTXHfZA2DrIbhLa4xbTCPZBsiUU9KI0A8ZCERfCdxIEWbNzFL788nQ76NId9rLyGIZAjyXgZBNaVUsQXPngbcT2sTsyGXwyDBITAYmioWrvwZDZD'
+	token = 'EAANcgmchBvsBAAqYdkAUwScmQLWNZAPsn3UidWPQbpCLMdqZAItlmDJpcVhu0x8ZBRunByJ0HGmJrxDdTEUYvaT1zCfALlSVH1VREHWyTZBl9POQutW6PfZCB8mEWTudOcREDN2QCkPMhBmSMIzJduuKWO5RZAfiZCU5rIJMkPHSF8IHQB66ZCq4zDeagfZA4As2sxa4ty7PMrAZDZD'
 
 	graph = facebook.GraphAPI(token)
 	profile = graph.get_object('me', fields='name,location{location}')
