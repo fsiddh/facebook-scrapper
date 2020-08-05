90fcbfb448d9540be4050d856ddf246ca887cb2e
fsiddh <jasonshaw546@gmail.com>
2020-08-05 10:22:37 +0530

    updates

--- Mining different kinds of data from fb/get_page_posts.py
+++ Mining different kinds of data from fb/get_page_posts.py
@@ -1,3 +1,49 @@
+# import os
+# import json
+# from argparse import ArgumentParser
+# import facebook
+# import requests
+
+# def get_parser():
+# 	parser = ArgumentParser()
+# 	parser.add_argument('--page')
+# 	parser.add_argument('--n', default=100, type=int)
+# 	return parser
+
+# if __name__ == '__main__':
+# 	parser = get_parser()
+# 	args = parser.parse_args()
+
+# 	token = 'EAANcgmchBvsBAEa65LtJIGWK9ouL6V8IsGQHcOkTB3QizSTD0KtCI6jBZCZBvvYLnd4UrNOnwBUop1rqWQ0UEp6hNd3myZC6GljtXCgBQobZBZBrSXLIiI4ZATUPvLFn138JCHDLwD7hpZAmBRpGR01eZBKRPCzZBIkkayOhUODEPagAKZAFHeUA9YH2C7ZC5Ck3Rmktxo7LeEohQZDZD'
+
+# 	graph = facebook.GraphAPI(token, version=3.0)
+# 	all_fields = [
+# 	'id',
+# 	'message',
+# 	'created_time',
+# 	'shares',
+# 	'likes.summary(true)',
+# 	'comments.summary(true)',
+# 	'reactions'
+# 	]
+# 	all_fields = ','.join(all_fields)
+# 	posts = graph.get_connections('PacktPub', 'posts', fields=all_fields)
+# 	downloaded = 0
+# 	while True: # keep paginating
+# 		if downloaded >= args.n:
+# 			break
+# 		try:
+# 			fname = 'posts_{}.jsonl'.format(args.page)
+# 			with open(fname, 'a') as f:
+# 				for post in posts['data']:
+# 					downloaded += 1
+# 					f.write(json.dumps(post)+"\n")
+# 				# get next page
+# 				posts = requests.get(posts['paging']['next']).json()
+# 		except KeyError:
+# 			# no more pages, break the loop
+# 			break
+
 import os
 import json
 from argparse import ArgumentParser
@@ -14,9 +60,8 @@ if __name__ == '__main__':
 	parser = get_parser()
 	args = parser.parse_args()
 
-	token = 'EAANcgmchBvsBAByxcHHnZChe0jXeTZCcxZArqaIWAsbgCZBACCK10SM9bMWMIX0NYUnkqTDGjFrZAQpgK3XeJejPNtBnv60IpEDWmTXHfZA2DrIbhLa4xbTCPZBsiUU9KI0A8ZCERfCdxIEWbNzFL788nQ76NId9rLyGIZAjyXgZBNaVUsQXPngbcT2sTsyGXwyDBITAYmioWrvwZDZD'
-
-	graph = facebook.GraphAPI(token, version=2.7)
+	token = 'EAANcgmchBvsBAL43EBt8mafG5pc7szkU5001lPubWrggFUxySOmbQRZAhzXpMkJgFEYhk0AzZBZBPU5ciIVJHXevI8YcxZBUB7XtXskvUETXZA3RunIaTf3swPU8mVVCyEJEJpLWf1Syxyqe8ZCnK35VzYHzaDVc1Fqvwz78nIi6ZC5ZCnOYauDVV9mqC7dwnC1DUqETB86jaYRb4tOSSKWmARL5vSG0UHZBlsTSMa8hZBUwZDZD'
+	graph = facebook.GraphAPI(token, version=3.1)
 	all_fields = [
 	'id',
 	'message',
@@ -27,7 +72,7 @@ if __name__ == '__main__':
 	'reactions'
 	]
 	all_fields = ','.join(all_fields)
-	posts = graph.get_connections('PacktPub', 'posts', fields=all_fields)
+	posts = graph.get_connections('teensMania', 'posts', fields=all_fields)
 	downloaded = 0
 	while True: # keep paginating
 		if downloaded >= args.n:
@@ -43,4 +88,3 @@ if __name__ == '__main__':
 		except KeyError:
 			# no more pages, break the loop
 			break
-
