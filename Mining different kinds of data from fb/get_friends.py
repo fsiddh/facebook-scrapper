f6fac4d639a5c6407fb5d8ad865b4763be04e9f2
fsiddh <jasonshaw546@gmail.com>
2020-08-18 16:45:49 +0530

    updates

--- Mining different kinds of data from fb/get_friends.py
+++ Mining different kinds of data from fb/get_friends.py
@@ -5,7 +5,7 @@ import facebook
 import json
 
 if __name__ == '__main__':
-	token = 'EAANcgmchBvsBAHY1sRrrEC8eFcZAfTW6QOGZAzBJHZCT9QFG8Wo7lufbb2ZA4Kn18gxBEwDjiRA4EVITQ4os6bitTdeqvNkORwvGforgh8vNimft1b6SwYrwnYZAq28EU0KW0zh2pJi8h8kUAkMZClaQMsKUoIz3GYNxBnZAc1HEX5PfBdkEd0X5XjBVMsQnETdwnjRKqagZBbazupIjcZBtJcWoACRmHiYzcfxvqHZBdMyQZDZD'
+	token = 'EAANcgmchBvsBACHZAxQKmaNVhRzNK5flowL1NMzQSjvUkrEEORgGwuL23pcY5sco0KmZBVLUi3eBKpwbIdHopL3LNJHC5EDE3aabltkbadSQcpOXQZAxS4gKIZCHrkq12DzPJXSrfMdcZAByA7ZBOeWKVlZClHIcnrEGc819uzjqTYV14jZCTEfeIJmPRnKoZAIIFqB1Y0urQ1tSfgLGRXftSaN1CRLvAUWfpZCGcc1uweBwZDZD'
 
 	graph = facebook.GraphAPI(token)
 	user = graph.get_object("me")
